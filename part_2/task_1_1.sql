eef302ba4359e683b9ecf43a622cd8c731996177
Sergey Kovalev <dipdeepcode@mail.ru>
2023-02-12 22:45:29 +0300

    task_1_1

--- part_2/task_1_1.sql
+++ part_2/task_1_1.sql
@@ -1,20 +1,15 @@
-WITH users_first_order_date AS (
-  SELECT
-    user_id,
-    MIN(time)::date AS first_order_date
-  FROM
-    user_actions
-  GROUP BY
-    user_id
-  ORDER BY
-    user_id
+WITH courier_action_2 AS (
+SELECT MIN(time)::date AS date, courier_id
+FROM courier_actions
+GROUP BY courier_id
+),
+user_action_2 AS (
+SELECT MIN(time)::date AS date, user_id
+FROM user_actions
+GROUP BY user_id
 )
-SELECT
-  COUNT(DISTINCT user_id),
-  first_order_date
-FROM
-  users_first_order_date
-GROUP BY
-  first_order_date
-ORDER BY
-  first_order_date;
+SELECT courier_action_2.date, COUNT(DISTINCT user_id) AS new_users, COUNT(DISTINCT courier_id) AS new_couriers
+FROM courier_action_2 FULL JOIN user_action_2 ON courier_action_2.date = user_action_2.date
+GROUP BY courier_action_2.date
+ORDER BY courier_action_2.date
+LIMIT 100
